b07a106ed6368e1afd21f37ab49d248ec8428042
mitul <tmitul515@gmail.com>
2016-03-19 21:26:07 +0600

    Second Commit

--- SpringProject/src/main/java/com/mitul/controller/newHomeController.java
+++ SpringProject/src/main/java/com/mitul/controller/newHomeController.java
@@ -7,7 +7,10 @@ import org.springframework.core.env.SystemEnvironmentPropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /**
@@ -29,5 +32,28 @@ public class newHomeController {
         return "test";
     }
 
+    @RequestMapping(value = "/newpost",method = RequestMethod.GET)
+    public String showNewPost(HttpSession session){
+        return "newPost";
+    }
+
+    @RequestMapping(value = "/submit",method = RequestMethod.POST)
+    public String submitNewPost(@RequestParam String username, @RequestParam String password,
+                                Model model, HttpSession session){
+
+
+
+        logininfo log = new logininfo();
+
+        log.setUsername(username);
+        log.setPassword(password);
+        int i = hom.insertPost(log);
+
+        if(i >=1) {
+            return "index";
+        }
+        else
+            return "error";
+    }
 
 }
